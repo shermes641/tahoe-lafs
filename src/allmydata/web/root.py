3e82818804abea321ef00ea96d22ca1d48ba853c
Brian Warner <warner@lothar.com>
2011-01-18 20:08:23 -0800

    add field for private notes, try to improve layout a bit

--- src/allmydata/web/root.py
+++ src/allmydata/web/root.py
@@ -153,6 +153,7 @@ class InvitationMaker(Resource):
         self.client = client
     def render_POST(self, request):
         petname = get_arg(request, "petname")
+        notes = get_arg(request, "notes")
         template = contents("generate-invitation2.html")
         return template % {"invite_code": "1234",
                            "petname": html.escape(petname),
@@ -165,6 +166,7 @@ class InvitationAcceptor(Resource):
     def render_POST(self, request):
         invite_code = get_arg(request, "invite_code")
         petname = get_arg(request, "petname")
+        notes = get_arg(request, "notes")
         template = contents("accept-invitation2.html")
         return template % {"petname": html.escape(petname)}
 
